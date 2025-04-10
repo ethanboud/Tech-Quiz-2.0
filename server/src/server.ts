141fba9f5343db585c5995698edb1a7710c363ed
ethanboud <ethanboud@gmail.com>
2025-04-09 20:51:00 -0600

    additional corrections for dirname for deployment

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,6 +4,12 @@ import path from 'node:path';
 import db from './config/connection.js';
 import routes from './routes/index.js';
 
+import { fileURLToPath } from 'url';
+
+// Define __dirname for ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 await db();
 
 const PORT = process.env.PORT || 3001;
