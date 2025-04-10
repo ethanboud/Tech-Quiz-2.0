e2fd7f2f78354d913be1de0dff82a9a43b27f1fe
ethanboud <ethanboud@gmail.com>
2025-04-10 00:03:58 -0600

    server corrections due to continuous deployment errors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,61 +1,19 @@
 import express from 'express';
-import path from 'node:path';
-
+// import path from 'node:path';
 import db from './config/connection.js';
 import routes from './routes/index.js';
 
-await db();
-
-const PORT = process.env.PORT || 3001;
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Serves static files in the entire client's dist folder
+app.use(express.static('../client/dist'));
+
 app.use(routes);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/dist')));
-
-   app.get('*', (_req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-  });
-}
-
-app.listen(PORT, () => {
-  console.log(`API server running on port ${PORT}!`);
+db.once('open', () => {
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
-
-
-
-// import express from 'express';
-// import path from 'node:path';
-
-// import db from './config/connection.js';
-// import routes from './routes/index.js';
-
-// import { fileURLToPath } from 'url';
-
-// // Define __dirname for ES modules
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-
-// await db();
-
-// const PORT = process.env.PORT || 10000;
-// const app = express();
-
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-// app.use(routes);
-
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../client/dist')));
-
-//    app.get('*', (_req, res) => {
-//     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-//   });
-// }
-
-// app.listen(PORT, () => {
-//   console.log(`API server running on port ${PORT}!`);
-// });
